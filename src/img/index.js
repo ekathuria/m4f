7d5240c6fd5bf1915b2202bcda95e6157c6ec999
E Kathuria <e@looklisten.com>
2020-06-06 00:10:18 -0400

    Clean-up + add album

---
+++ src/img/index.js
@@ -0,0 +1,5 @@
+export const img1 = require('./1.jpg');
+export const img2 = require('./2.jpg');
+export const img3 = require('./3.jpg');
+export const img4 = require('./4.jpg');
+export const img5 = require('./5.jpg');
