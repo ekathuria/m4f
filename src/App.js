86ce9a4ad48d5a02d23157a5ca989f19bea0f35f
E Kathuria <e@looklisten.com>
2020-06-07 12:59:26 -0400

    Add albums component + misc changes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import React from 'react';
-import {img1, img2, img3} from './img/index';
 import './App.css';
+import Albums from './components/Albums';
 
-const albums = [
-  {
-    track: 1,
-    title: "Reckoner",
-    album: "In Rainbows",
-    cover: img1
-  }
-]
 
 function App() {
   return (
     <div className="App">
-        <img src={albums[0].cover}  />
+      <Albums />
     </div>
   );
 }
